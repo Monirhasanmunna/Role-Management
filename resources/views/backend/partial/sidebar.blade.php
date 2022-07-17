0d5bafa73299b73cc5c922f4277aa690bafde87a
munna <monirhasanmunna1@gmail.com>
2022-07-18 00:44:18 +0600

    project create admin panel partial done

---
+++ resources/views/backend/partial/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="quixnav">
+    <div class="quixnav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label first">Main Menu</li>
+            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false"><i
+                        class="material-symbols-outlined">
+                        dashboard
+                    </i><span class="nav-text">Dashboard</span></a>
+                <ul aria-expanded="false">
+                    <li><a href="./index.html">Dashboard 1</a></li>
+                    <li><a href="./index2.html">Dashboard 2</a></li>
+                </ul>
+            </li>
+            
+        </ul>
+    </div>
+
+
+</div>
